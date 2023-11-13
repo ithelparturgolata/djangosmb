ef399e405a28dd777b28c3d873d26ac6c5bf979c
Arrtur Golata <ithelparturgolata@gmail.com>
2023-11-13 07:40:20 +0100

    dodalem pakiet django-auto-logout

--- djangosmb/settings.py
+++ djangosmb/settings.py
@@ -2,13 +2,10 @@ import os.path
 from pathlib import Path
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -46,6 +43,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_auto_logout.middleware.auto_logout',
 ]
 
 ROOT_URLCONF = "djangosmb.urls"
@@ -62,6 +60,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django_auto_logout.context_processors.auto_logout_client",
             ],
         },
     },
@@ -119,7 +118,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
 STATIC_ROOT = BASE_DIR / 'static_root'
 
 MEDIA_URL = '/media/'
@@ -130,6 +128,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+AUTO_LOGOUT = {
+    'IDLE_TIME': 6000,
+    'REDIRECT_TO_LOGIN_IMMEDIATELY': True,
+    'MESSAGE': 'Sesja wygasła. Aby kontynuować, zaloguj się ponownie.',
+}
 
 
