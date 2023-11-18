d15bed76b2ddba97c42ed224b8f0bc6ddaaa81ff
Arrtur Golata <ithelparturgolata@gmail.com>
2023-11-18 13:54:02 +0100

    poprawki urls dla telefony

--- telefony/forms.py
+++ telefony/forms.py
@@ -25,21 +25,16 @@ class AddRecordFormTelefony(forms.ModelForm):
 
 
 class UpdateRecordFormTelefony(forms.ModelForm):
-    indeks = forms.CharField(label="Indeks", max_length=7)
     nazwa = forms.CharField(label="Nazwisko i Imię", max_length=255)
-    adres = forms.CharField(label="Data pozwu", max_length=255)
     klatka = forms.CharField(label="Klatka",
                              max_length=2)
     telefon = forms.CharField(label="Telefon",
                              max_length=9)
     zgoda = forms.CharField(label="Zgoda tak/nie",
                                   max_length=3)
-    administracja = forms.CharField(label="Administracja",
-                            max_length=2)
-
     class Meta:
         model = Mieszkaniec
-        fields = ["indeks", "nazwa", "adres", "klatka", "telefon", "zgoda", "administracja"]
+        fields = ["nazwa", "klatka", "telefon", "zgoda"]
 
 
 class SmsRecordFormTelefony(forms.ModelForm):
